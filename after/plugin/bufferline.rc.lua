8f1364241c82cddd4a15a33722c0f88c6937e6b1
lizard.liang <lizard.liang@watchsoft.com.tw>
2023-05-02 16:18:07 +0800

    feat: new bufferline style

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -1,6 +1,7 @@
 local status, bufferline = pcall(require, "bufferline")
 if (not status) then return end
 
+
 bufferline.setup({
   options = {
     mode = "tabs",
@@ -12,22 +13,22 @@ bufferline.setup({
   },
   highlights = {
     separator = {
-      fg = '#073642',
-      bg = '#002b36',
+      fg = '#2c3043',
+      bg = '#363646',
     },
     separator_selected = {
-      fg = '#073642',
+      fg = '#2c3043',
     },
     background = {
       fg = '#657b83',
-      bg = '#002b36'
+      bg = '#363646'
     },
     buffer_selected = {
       fg = '#fdf6e3',
       bold = true,
     },
     fill = {
-      bg = '#073642'
+      bg = '#2c3043'
     }
   },
 })
