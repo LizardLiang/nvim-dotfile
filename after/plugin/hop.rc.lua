5814a7b13a8e81f51c03a475cdffa31898fcb1e0
LizardLiang <shotup0101@gmail.com>
2023-05-05 21:37:37 +0800

    feat: add which key

--- after/plugin/hop.rc.lua
+++ after/plugin/hop.rc.lua
@@ -1,14 +1,14 @@
-local hop = require('hop')
-local directions = require('hop.hint').HintDirection
-local keymap = vim .keymap
+local hop = require("hop")
+local directions = require("hop.hint").HintDirection
+local keymap = vim.keymap
 
 hop.setup({})
 
-keymap.set('n', 'f', function()
-  hop.hint_char1({direction=directions.AFTER_CURSOR})
-end)
-keymap.set('n', 'F', function()
-  hop.hint_char1({direction=directions.BEFORE_CURSOR})
-end)
-keymap.set('n', '<leader>hw',"<Cmd>HopWord<CR>")
-keymap.set('n', '<leader>hl', "<Cmd>HopLineStart<CR>")
+keymap.set("n", "f", function()
+	hop.hint_char1({ direction = directions.AFTER_CURSOR })
+end, { desc = "Find char after cursor" })
+keymap.set("n", "F", function()
+	hop.hint_char1({ direction = directions.BEFORE_CURSOR })
+end, { desc = "Find char before cursor" })
+keymap.set("n", "<leader>hw", "<Cmd>HopWord<CR>", { desc = "Find Word In Current Buffer" })
+keymap.set("n", "<leader>hl", "<Cmd>HopLineStart<CR>", { desc = "Find Line Start Key" })
--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -1,106 +1,111 @@
 local status, telescope = pcall(require, "telescope")
-if (not status) then return end
-local actions = require('telescope.actions')
+if not status then
+	return
+end
+local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
 local trouble = require("trouble.providers.telescope")
 
 local function telescope_buffer_dir()
-  return vim.fn.expand('%:p:h')
+	return vim.fn.expand("%:p:h")
 end
 
-local fb_actions = require "telescope".extensions.file_browser.actions
+local fb_actions = require("telescope").extensions.file_browser.actions
 
-telescope.setup {
-  defaults = {
-    mappings = {
-      n = {
-        ["q"] = actions.close,
-      },
-      i = {
-        ["<C-h>"] = "which_key"
-      },
-    },
-  },
-  file_ignore_patterns = {
-    "./node_modules/*", "node_modules", "^node_modules/*", "node_modules/*"
-  },
-  extensions = {
-    file_browser = {
-      theme = "dropdown",
-      -- disables netrw and use telescope-file-browser in its place
-      hijack_netrw = true,
-      mappings = {
-        -- your custom insert mode mappings
-        ["i"] = {
-          ["<C-w>"] = function() vim.cmd('normal vbd') end,
-        },
-        ["n"] = {
-          -- your custom normal mode mappings
-          ["N"] = fb_actions.create,
-          ["h"] = fb_actions.goto_parent_dir,
-          ["/"] = function()
-            vim.cmd('startinsert')
-          end
-        },
-      },
-    },
-  },
-}
+telescope.setup({
+	defaults = {
+		mappings = {
+			n = {
+				["q"] = actions.close,
+				["<leader>w"] = "which_key",
+			},
+		},
+	},
+	file_ignore_patterns = {
+		"./node_modules/*",
+		"node_modules",
+		"^node_modules/*",
+		"node_modules/*",
+	},
+	extensions = {
+		file_browser = {
+			theme = "dropdown",
+			-- disables netrw and use telescope-file-browser in its place
+			hijack_netrw = true,
+			mappings = {
+				-- your custom insert mode mappings
+				["i"] = {
+					["<C-w>"] = function()
+						vim.cmd("normal vbd")
+					end,
+				},
+				["n"] = {
+					-- your custom normal mode mappings
+					["N"] = fb_actions.create,
+					["h"] = fb_actions.goto_parent_dir,
+					["/"] = function()
+						vim.cmd("startinsert")
+					end,
+				},
+			},
+		},
+	},
+})
 
 telescope.load_extension("file_browser")
 
-vim.keymap.set('n', '\\\\', function()
-  builtin.buffers()
-end)
+vim.keymap.set("n", "\\\\", function()
+	builtin.buffers()
+end, { desc = "Telescope Find Buffer" })
 vim.keymap.set("n", "<leader>fh", function()
-  builtin.help_tags()
-end)
-vim.keymap.set('n', "<leader>fd", function(...)
-  builtin.diagnostics()
-  -- trouble.open_with_trouble(...)
+	builtin.help_tags()
+end, { desc = "Telescope Find Tags" })
+vim.keymap.set("n", "<leader>fd", function(...)
+	builtin.diagnostics()
+	-- trouble.open_with_trouble(...)
 end)
 vim.keymap.set("n", "sf", function()
-  telescope.extensions.file_browser.file_browser({
-    path = "%:p:h",
-    cwd = telescope_buffer_dir(),
-    respect_gitignore = false,
-    hidden = true,
-    grouped = true,
-    previewer = false,
-    initial_mode = "normal",
-    layout_config = { height = 40 }
-  })
+	telescope.extensions.file_browser.file_browser({
+		path = "%:p:h",
+		cwd = telescope_buffer_dir(),
+		respect_gitignore = false,
+		hidden = true,
+		grouped = true,
+		previewer = false,
+		initial_mode = "normal",
+		layout_config = { height = 40 },
+	})
 end)
 vim.keymap.set("n", "<leader>fo", function()
-  builtin.oldfiles()
+	builtin.oldfiles()
 end)
 vim.keymap.set("n", "<leader>fs", function()
-  builtin.lsp_document_symbols()
+	builtin.lsp_document_symbols()
 end)
 vim.keymap.set("n", "<leader>fb", function()
-  builtin.current_buffer_fuzzy_find()
+	builtin.current_buffer_fuzzy_find()
 end)
 vim.keymap.set("n", "<leader>ft", function()
-  builtin.lsp_type_definitions()
+	builtin.lsp_type_definitions()
 end)
 vim.keymap.set("n", "<leader>sg", function()
-  builtin.live_grep()
+	builtin.live_grep()
 end)
 vim.keymap.set("n", "<leader>fgs", function()
-  builtin.git_status()
+	builtin.git_status()
 end)
 vim.keymap.set("n", "<leader>fgc", function()
-  builtin.git_commits()
+	builtin.git_commits()
 end)
 vim.keymap.set("n", "<leader>st", function()
-  vim.cmd("TodoTelescope")
+	vim.cmd("TodoTelescope")
 end)
-vim.keymap.set("n", "<leader>fc", function() 
-  builtin.grep_string()
+vim.keymap.set("n", "<leader>fc", function()
+	builtin.grep_string()
 end)
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
 -- vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
