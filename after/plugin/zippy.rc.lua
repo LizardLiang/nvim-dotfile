788758be8285c8aa4f40e788512fb024928a15a3
LizardLiang <shotup0101@gmail.com>
2023-05-06 03:06:28 +0000

    feat: modified python debug print function

--- after/plugin/zippy.rc.lua
+++ after/plugin/zippy.rc.lua
@@ -1,2 +1,9 @@
+local zippy = require("zippy")
+
+zippy.setup({
+	["python"] = {
+		print_function = "logger.debug",
+	},
+})
 
 vim.keymap.set("n", "clg", "<cmd>lua require('zippy').insert_print()<CR>")
