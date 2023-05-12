db9e04a0b57b5bf942a1c29156c528ee4ad4ff8d
lizard.liang <lizard.liang@watchsoft.com.tw>
2023-05-12 09:57:38 +0800

    feat: add support for wsl, remove unecessary setup

--- init.lua
+++ init.lua
@@ -2,7 +2,17 @@ require("lizard.base")
 require("lizard.highlights")
 require("lizard.map")
 require("lizard.plugins")
-require("lizard.windows")
+
+local has = vim.fn.has
+local is_win = has "win32"
+local is_wsl = has "wsl"
+
+if is_win == 1 then
+  require("lizard.windows")
+end
+if is_wsl == 1 then
+  require("lizard.wsl")
+end
 
 -- See `:help vim.o`
 
@@ -144,42 +154,11 @@ local on_attach = function(_, bufnr)
   end, { desc = "Format current buffer with LSP" })
 end
 
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
-local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
-}
-
--- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
 -- Setup mason so it can manage external tooling
 require("mason").setup()
 
 -- Ensure the servers above are installed
 local mason_lspconfig = require("mason-lspconfig")
-
-mason_lspconfig.setup({
-  ensure_installed = vim.tbl_keys(servers),
-})
-
-mason_lspconfig.setup_handlers({
-  function(server_name)
-    require("lspconfig")[server_name].setup({
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-    })
-  end,
-})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
