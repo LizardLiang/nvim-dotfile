e64c25d525a39482b0690ccda202864efd40b48f
LizardLiang <shotup0101@gmail.com>
2023-05-13 01:38:38 +0000

    feat: remap keybind

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -105,7 +105,7 @@ vim.keymap.set("n", "<leader>fc", function()
 	builtin.grep_string()
 end, { desc = "[F]ind [C]ode" })
 
-vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
+vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "[F]ind [F]iles" })
 vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
 vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
 -- vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
