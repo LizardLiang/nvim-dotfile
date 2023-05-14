ec2026f748445c3e6a086178723bf419a7781bf2
LizardLiang <shotup0101@gmail.com>
2023-05-14 12:55:46 +0800

    fix: pylint is not start automatically

--- plugin/0_mason.rc.lua
+++ plugin/0_mason.rc.lua
@@ -1,12 +1,21 @@
 local status, mason = pcall(require, "mason")
-if (not status) then return end
+if not status then
+	return
+end
 local status2, lspconfig = pcall(require, "mason-lspconfig")
-if (not status2) then return end
+if not status2 then
+	return
+end
 
-mason.setup({
+mason.setup({})
 
+lspconfig.setup({
+	automatic_installation = true,
 })
 
-lspconfig.setup {
-  automatic_installation = true
-}
+lspconfig.setup_handlers({
+
+	function(server_name) -- default handler (optional)
+		require("lspconfig")[server_name].setup({})
+	end,
+})
