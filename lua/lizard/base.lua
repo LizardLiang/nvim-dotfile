091200cb3af3838467fba17b4faf0bb6c723ca4b
LizardLiang <shotup0101@gmail.com>
2023-07-21 12:19:14 +0000

    feat: remove duplicate settings

--- lua/lizard/base.lua
+++ lua/lizard/base.lua
@@ -58,11 +58,11 @@ vim.g.netrw_winsize = 20
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-	group = highlight_group,
-	pattern = "*",
-})
+-- local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+-- vim.api.nvim_create_autocmd("TextYankPost", {
+-- 	callback = function()
+-- 		vim.highlight.on_yank()
+-- 	end,
+-- 	group = highlight_group,
+-- 	pattern = "*",
+-- })
