5b5d621815a93e4d731281175b387bb03fea4d31
LizardLiang <shotup0101@gmail.com>
2023-07-22 19:54:05 +0800

    feat: fix treesitter problem

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,104 +1,65 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	build = function()
-		require("nvim-treesitter.install").update({ with_sync = true })
-	end,
-	event = "BufRead",
-	config = function()
-		local status, ts = pcall(require, "nvim-treesitter.configs")
-		if not status then
-			return
+	build = ":TSUpdate",
+	dependencies = {
+		{
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			init = function()
+				-- disable rtp plugin, as we only need its queries for mini.ai
+				-- In case other textobject modules are enabled, we will load them
+				-- once nvim-treesitter is loaded
+				require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+				load_textobjects = true
+			end,
+		},
+	},
+	event = { "BufReadPost", "BufNewFile" },
+	cmd = { "TSUpdateSync" },
+	config = function(_, opts)
+		if type(opts.ensure_installed) == "table" then
+			---@type table<string, boolean>
+			local added = {}
+			opts.ensure_installed = vim.tbl_filter(function(lang)
+				if added[lang] then
+					return false
+				end
+				added[lang] = true
+				return true
+			end, opts.ensure_installed)
 		end
-
-		ts.setup({})
-
-		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-		parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
-
-		-- [[ Configure Treesitter ]]
-		-- See `:help nvim-treesitter`
-		require("nvim-treesitter.configs").setup({
-			-- Add languages to be installed here that you want installed for treesitter
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<c-space>",
-					node_incremental = "<c-space>",
-					scope_incremental = "<c-s>",
-					node_decremental = "<c-backspace>",
-				},
-			},
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						["aa"] = "@parameter.outer",
-						["ia"] = "@parameter.inner",
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
-					},
-				},
-				move = {
-					enable = true,
-					set_jumps = true, -- whether to set jumps in the jumplist
-					goto_next_start = {
-						["]m"] = "@function.outer",
-						["]]"] = "@class.outer",
-					},
-					goto_next_end = {
-						["]M"] = "@function.outer",
-						["]["] = "@class.outer",
-					},
-					goto_previous_start = {
-						["[m"] = "@function.outer",
-						["[["] = "@class.outer",
-					},
-					goto_previous_end = {
-						["[M"] = "@function.outer",
-						["[]"] = "@class.outer",
-					},
-				},
-				swap = {
-					enable = true,
-					swap_next = {
-						["<leader>a"] = "@parameter.inner",
-					},
-					swap_previous = {
-						["<leader>A"] = "@parameter.inner",
-					},
-				},
-				highlight = {
-					enable = true,
-					disable = {},
-				},
-				indent = {
-					enable = true,
-					disable = {},
-				},
-				ensure_installed = {
-					"markdown",
-					"markdown_inline",
-					"tsx",
-					"toml",
-					"json",
-					"yaml",
-					"css",
-					"html",
-					"lua",
-					"c",
-					"cpp",
-					"python",
-					"typescript",
-				},
-				autotag = {
-					enable = true,
-				},
-				disable = { "cs" },
-			},
-		})
+		require("nvim-treesitter.configs").setup(opts)
 	end,
+	opts = {
+		highlight = { enable = true },
+		indent = { enable = true },
+		ensure_installed = {
+			"bash",
+			"c",
+			"html",
+			"javascript",
+			"json",
+			"lua",
+			"luadoc",
+			"luap",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"query",
+			"regex",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"yaml",
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-space>",
+				node_incremental = "<C-space>",
+				scope_incremental = false,
+				node_decremental = "<bs>",
+			},
+		},
+	},
 }
