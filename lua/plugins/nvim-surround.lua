c48e15c8605427cd167c9f22f44aa938e955181b
lizard.liang <lizard.liang@watchsoft.com.tw>
2023-09-14 11:20:02 +0800

    feat: add d as trigger command for surround

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,6 +1,6 @@
 return {
 	"kylechui/nvim-surround",
-	keys = { "s", "c", "y", "v" },
+	keys = { "d", "c", "y", "v" },
 	config = function()
 		require("nvim-surround").setup({
 			-- Configuration here, or leave empty to use defaults
