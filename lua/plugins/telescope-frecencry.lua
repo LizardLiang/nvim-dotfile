ab11e3b47baa28751279bbf05a9de5a67a02a91f
lizard.liang <lizard.liang@watchsoft.com.tw>
2023-11-13 11:28:25 +0800

    feat(telescop): remove frecency

--- lua/plugins/telescope-frecencry.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "nvim-telescope/telescope-frecency.nvim",
-  keys = { { "<leader><leader>", "<Cmd>Telescope frecency workspace=CWD<CR>" } },
-  config = function()
-    require("telescope").load_extension("frecency")
-  end,
-}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -25,6 +25,8 @@ return {
     local telescope = require("telescope")
 
     local actions = require("telescope.actions")
+    local builtin = require("telescope.builtin")
+    local trouble = require("trouble.providers.telescope")
 
     local function telescope_buffer_dir()
       return vim.fn.expand("%:p:h")
