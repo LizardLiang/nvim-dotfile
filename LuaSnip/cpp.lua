6335625947b697bba4b0d14a1b3ba21271a3e129
lizard.liang <lizard.liang@watchsoft.com.tw>
2023-11-14 15:33:47 +0800

    feat(ufo): add nvim-ufo

--- LuaSnip/cpp.lua
+++ LuaSnip/cpp.lua
@@ -1,6 +1,7 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
 
 return {
   s({ trig = "doc" }, {
@@ -8,5 +9,10 @@ return {
       "/**************************************************",
       "**************************************************/",
     }),
+  }),
+  s({ trig = "sec" }, {
+    t({ "/**************** " }),
+    i(1),
+    t({ " *****************/" }),
   }),
 }
