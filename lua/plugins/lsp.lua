87981a4547e1ea3e4fa59933f42a993b144c48f9
lizard.liang <lizard.liang@watchsoft.com.tw>
2024-01-12 11:01:31 +0800

    feat(mason): remove unusable linter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,7 +4,6 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "stylua",
-        "luacheck",
         "shellcheck",
         "shfmt",
         "typescript-language-server",
