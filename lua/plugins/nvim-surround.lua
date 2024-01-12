6f2d57fe32d27be7189f62a3056f887d9093fb30
lizard.liang <lizard.liang@watchsoft.com.tw>
2024-01-12 10:49:21 +0800

    feat(harpoon): add harpoon

--- lua/plugins/nvim-surround.lua
+++ lua/plugins/nvim-surround.lua
@@ -1,6 +1,7 @@
 return {
   "kylechui/nvim-surround",
   keys = { "d", "c", "y", "v" },
+  vscode = true,
   config = function()
     require("nvim-surround").setup({
       -- Configuration here, or leave empty to use defaults
