19529d75d12b3a7880a4272748b1204b27d9ff58
LizardLiang <shotup0101@gmail.com>
2024-03-21 21:36:27 +0800

    feat(code snapshot): 設定預設主題及padding

--- lua/plugins/ray.lua
+++ lua/plugins/ray.lua
@@ -4,4 +4,10 @@ return {
   keys = {
     { "<leader>cs", ":Ray<CR>", desc = "[C]ode [S]napshot", noremap = true, silent = true, mode = { "n", "v" } },
   },
+  config = function()
+    vim.g.ray_options = {
+      padding = "16",
+      theme = "midnight",
+    }
+  end,
 }
